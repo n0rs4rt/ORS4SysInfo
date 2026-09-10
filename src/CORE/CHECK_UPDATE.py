042160d69d515b06f1ffeacb141d85a24d04c077
Nelson Arteaga <arteagapt92@gmail.com>
2026-09-10 23:41:15 +0100

    Add files via upload

--- src/CORE/CHECK_UPDATE.py
+++ src/CORE/CHECK_UPDATE.py
@@ -3,7 +3,7 @@ import urllib.request, json
 def comprobar_actualizacion ():
     try:
         url = "https://api.github.com/repos/n0rs4rt/ORS4SysInfo/releases/latest"
-        version_actual = "1.2.0"
+        version_actual = "v1.2.0"
         with urllib.request.urlopen(url) as response:
             contenido = response.read().decode("utf-8")
             contenido = json.loads(contenido)
@@ -16,4 +16,3 @@ def comprobar_actualizacion ():
                 return False
     except:
         return
-        
